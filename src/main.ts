8e9755914c2ff09393902732a1f1ccd6677997a3
LayneTheEpic <layne.ogden727@gmail.com>
2022-08-19 10:45:38 -0400

    22/8/19

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import {scale} from "./util.js";
+import {randomBetween, scale} from "./util.js";
 
 
 
@@ -35,27 +35,49 @@ async function main() {
 	const source = audioContext.createMediaElementSource(audio);
 
 
-	const analyzer = audioContext.createAnalyser();
-	analyzer.fftSize = 512; // default: 1024
+	const frequencyAnalyzer = audioContext.createAnalyser();
+	frequencyAnalyzer.fftSize = 512; // default: 2048
+
+	const beatAnalyzer = audioContext.createAnalyser();
+	const filter = audioContext.createBiquadFilter();
+	filter.type = "highpass";
 
 
-	source.connect(analyzer).connect(audioContext.destination);
+	source.connect(filter).connect(audioContext.destination);
+	source.connect(filter).connect(frequencyAnalyzer);
+	// source.connect(beatAnalyzer);
 
 	audio.play();
 
 
-	requestAnimationFrame(() => animateCanvas(audioContext, analyzer));
+	// ctx.translate(0, height);
+	ctx.transform(1, 0, 0, -1, 0, height);
+	requestAnimationFrame(() => processAudio(audioContext, frequencyAnalyzer, beatAnalyzer));
+}
+
+
+
+function processAudio(audioContext: AudioContext, frequencyAnalzyer: AnalyserNode, beatAnalyzer: AnalyserNode) {
+	// drawCanvasBg();
+	animateCanvas(audioContext, frequencyAnalzyer);
+}
+
+
+
+function drawCanvasBg() {
+	// ctx.fillStyle = `hsl(${randomBetween(0, 360)}, 58%, 53%)`;
+
 }
 
 
 
 function animateCanvas(audioContext: AudioContext, analyzer: AnalyserNode) {
-	const bars = 256; // 256 * (44100 / 512) = 22050; max of human hearing
+	const bars = analyzer.fftSize / 2; // 256 * (44100 / 512) = 22050; max of human hearing
 
 	const bufferLength = analyzer.frequencyBinCount;
 
 	// data[n] = n * 44100/fftSize (in Hz)
-	const rawData = new Uint8Array(analyzer.frequencyBinCount);
+	const rawData = new Uint8Array(bufferLength);
 	const data = rawData.subarray(0, bars);
 
 	analyzer.getByteFrequencyData(data); // this is more like a "copyByteDataToArray"
@@ -64,15 +86,33 @@ function animateCanvas(audioContext: AudioContext, analyzer: AnalyserNode) {
 	ctx.fillStyle = "#000";
 	ctx.fillRect(0, 0, width, height);
 
-
 	const barWidth = Math.floor(width / bars);
 	ctx.fillStyle = "#eee";
+	ctx.strokeStyle = "#eee";
+	ctx.lineWidth = 2;
+
 
 	for(let i = 0; i < bars; i++) {
 		const scaledBar = Math.round(scale(data[i], 0, 255, 0, height));
 
+		if(i === 0) {
+			ctx.beginPath();
 
-		ctx.fillRect(i * barWidth, height - scaledBar, barWidth, scaledBar)
+			ctx.moveTo(0, 0);
+			ctx.lineTo(0, scaledBar);
+			ctx.lineTo(barWidth - 1, scaledBar);
+
+			continue;
+		}
+
+
+		ctx.lineTo(i * barWidth, scaledBar);
+		ctx.lineTo((i + 1) * barWidth - 1, scaledBar);
+
+
+		if(i === bars - 1) {
+			ctx.stroke();
+		}
 	}
 
 
