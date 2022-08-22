7b4641cdcef246e007fa092d98f161e77dc1ed3b
LayneTheEpic <layne.ogden727@gmail.com>
2022-08-22 10:48:55 -0400

    22/8/22 - begin to restructure code base in order to add beat detection

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import {randomBetween, scale} from "./util.js";
+import visualizeAudio from "./audioVisualizer.js";
 
 
 
@@ -24,43 +24,80 @@ async function main() {
 
 	if(!file || !file.type.includes("audio/")) return;
 
-
 	fileButton.classList.add("hide");
+
+
 
 	const audioContext = new AudioContext();
 
-	const url = URL.createObjectURL(file); // turn the file contents into something accessible
-	const audio = new Audio(url);
+	// Create audioSource
+	const audioUrl = URL.createObjectURL(file); // turn the file contents into something accessible
+	const audioElement = new Audio(audioUrl);
 
-	const source = audioContext.createMediaElementSource(audio);
+	const audioSource = audioContext.createMediaElementSource(audioElement);
+
+
+	// Create offlineContext (this will calculate where beats land in the background)
+	const audioLength = await new Promise(resolve => {
+		audioElement.addEventListener("durationchange", () => {
+			resolve(audioElement.duration);
+		}, {once: true});
+	}) as number;
+
+	// alert(Math.round(audio.duration * 44100));
+
+	const offlineContext = new OfflineAudioContext({
+		numberOfChannels: 2,
+		length: Math.ceil(audioLength * 44100),
+		sampleRate: 44100
+	});
+
+	// hgkjdfkgdkdkdfdfjkgfddfgjkfdgjkffdjkfkjd
+	// offlineContext.buffer =
+
+
+	// source.
+
+
+
+
+
+
 
 
 	const frequencyAnalyzer = audioContext.createAnalyser();
 	frequencyAnalyzer.fftSize = 512; // default: 2048
+	// frequencyAnalyzer.smoothingTimeConstant = 0;
 
 	const beatAnalyzer = audioContext.createAnalyser();
+	beatAnalyzer.smoothingTimeConstant = 0; // easier to analyze values
+
 	const filter = audioContext.createBiquadFilter();
-	filter.type = "highpass";
+	filter.type = "lowpass";
 
 
-	source.connect(filter).connect(audioContext.destination);
-	source.connect(filter).connect(frequencyAnalyzer);
-	// source.connect(beatAnalyzer);
+	audioSource.connect(audioContext.destination);
+	// source.connect(frequencyAnalyzer);
+	audioSource.connect(filter).connect(frequencyAnalyzer);
+	audioSource.connect(filter).connect(beatAnalyzer);
 
-	audio.play();
+	audioElement.play();
+
+
+
 
 
 	// ctx.translate(0, height);
-	ctx.transform(1, 0, 0, -1, 0, height);
-	requestAnimationFrame(() => processAudio(audioContext, frequencyAnalyzer, beatAnalyzer));
+	// ctx.transform(1, 0, 0, -1, 0, height);
+	// requestAnimationFrame(() => processAudio(audioContext, frequencyAnalyzer, beatAnalyzer));
 }
 
 
 
-function processAudio(audioContext: AudioContext, frequencyAnalzyer: AnalyserNode, beatAnalyzer: AnalyserNode) {
-	// drawCanvasBg();
-	animateCanvas(audioContext, frequencyAnalzyer);
-}
+// function processAudio(audioContext: AudioContext, frequencyAnalzyer: AnalyserNode, beatAnalyzer: AnalyserNode) {
+// 	// drawCanvasBg();
+// 	visualizeAudio(audioContext, frequencyAnalzyer);
+// }
 
 
 
@@ -71,60 +108,14 @@ function drawCanvasBg() {
 
 
 
-function animateCanvas(audioContext: AudioContext, analyzer: AnalyserNode) {
-	const bars = analyzer.fftSize / 2; // 256 * (44100 / 512) = 22050; max of human hearing
-
-	const bufferLength = analyzer.frequencyBinCount;
-
-	// data[n] = n * 44100/fftSize (in Hz)
-	const rawData = new Uint8Array(bufferLength);
-	const data = rawData.subarray(0, bars);
-
-	analyzer.getByteFrequencyData(data); // this is more like a "copyByteDataToArray"
-
-
-	ctx.fillStyle = "#000";
-	ctx.fillRect(0, 0, width, height);
-
-	const barWidth = Math.floor(width / bars);
-	ctx.fillStyle = "#eee";
-	ctx.strokeStyle = "#eee";
-	ctx.lineWidth = 2;
-
-
-	for(let i = 0; i < bars; i++) {
-		const scaledBar = Math.round(scale(data[i], 0, 255, 0, height));
-
-		if(i === 0) {
-			ctx.beginPath();
-
-			ctx.moveTo(0, 0);
-			ctx.lineTo(0, scaledBar);
-			ctx.lineTo(barWidth - 1, scaledBar);
-
-			continue;
-		}
-
-
-		ctx.lineTo(i * barWidth, scaledBar);
-		ctx.lineTo((i + 1) * barWidth - 1, scaledBar);
-
-
-		if(i === bars - 1) {
-			ctx.stroke();
-		}
-	}
-
-
-	requestAnimationFrame(() => animateCanvas(audioContext, analyzer));
-}
-
-
-
 /*
 
 Ideas:
 
 beat detection changes bg (hsl)
+
+
+implementation:
+	- preload audio into offlinecontext, process, identify peak
 
 */
